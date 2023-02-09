eb45be94f0b33771e843529783101198d03c0a49
Paras Gediya <mrparas103@gmail.com>
2023-02-09 20:06:47 +0530

    change navbar ui for mobile divices

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import SearchPokemon from "./SearchPokemon";
 const Navbar = () => {
   return (
     <nav className="h-16 fixed z-50 top-0 left-0 right-0 glass ">
-      <div className="p-0 flex items-center justify-between h-full app-container">
+      <div className="max-sm:p-2 p-0 max-sm:w-full flex items-center justify-between h-full app-container">
         <Link
           className="text-transparent bg-clip-text bg-gradient-to-br from-fuchsia-500  to-violet-500 font-extrabold text-2xl"
           to="/"
