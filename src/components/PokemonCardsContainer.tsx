f3f4e010c3bdbf05a1229006a1381f468a32829d
Paras Gediya <mrparas103@gmail.com>
2023-02-11 17:52:15 +0530

    add button to display powers in home page header

--- src/components/PokemonCardsContainer.tsx
+++ src/components/PokemonCardsContainer.tsx
@@ -10,9 +10,18 @@ const PokemonCardsContainer = () => {
       <div className="flex flex-col gap-3">
         <div className="glass p-2 rounded-md flex items-center justify-between">
           <h2 className="font-semibold text-xl">All Pokemons</h2>
-          <Link to={"/pokemons/new"}>
-            <button className="btn btn--gradient rounde-md">Add Pokemon</button>
-          </Link>
+          <div className="space-x-2">
+            <Link to={"/powers"}>
+              <button className="btn btn--gradient rounde-md">
+                All Powers
+              </button>
+            </Link>
+            <Link to={"/pokemons/new"}>
+              <button className="btn btn--gradient rounde-md">
+                Add Pokemon
+              </button>
+            </Link>
+          </div>
         </div>
         <div className="grid grid-flow-row grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {pokemons.map((currPokemon, idx) => (
